25ed11f29ebace35f0f85cb6d2a33f5b70ff4e26
Omar Trejo <otrenav@gmail.com>
2015-05-08 21:41:32 -0500

    Archivo inivial para proyecto final.

---
+++ Trabajo_final/trabajo_final.m
@@ -0,0 +1,42 @@
+%
+% Trabajo final - Macroeconomía Dinámica
+% Profesor: Carlos Urrutia
+% ITAM, 2015
+%
+% Equipo:
+% Arturo Reynoso APELLIDO, 000000
+% Alejandro Cerecero APELLIDO, 000000
+% Omar Trejo Navarro, 119711
+%
+
+%% Inciso a
+
+% Parámetros
+beta       = 0.987
+gamma      = 0.64
+alpha      = 0.40
+delta      = 0.012
+rho        = 0.95
+sigma_eps  = 0.0024
+sigma_zeta = 0.007
+
+%% Inciso b
+
+% Cinco estados para el shock
+Z = [=0.0231 -0.0115 0.0115 0.0231]
+
+% Matriz de transición
+Pi = [0.9727 0.0273      0      0      0;
+      0.0041 0.9806 0.0153      0      0;
+           0 0.0082 0.9837 0.0082      0;
+           0      0 0.0153 0.9806 0.0041;
+           0      0      0 0.0273 0.9727];
+
+
+%% Inciso c
+
+
+
+%% Inciso d
+
+
